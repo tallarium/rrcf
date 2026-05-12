dc472ced9cd09b182d81051cf85f60403e25c7f5
KieranMontgomery <kieran@tallarium.com>
2026-05-12 10:58:39 +0100

    Convert project to UV project

--- rrcf/__init__.py
+++ rrcf/__init__.py
@@ -1,5 +1,2 @@
 from rrcf.rrcf import *
 from rrcf.shingle import shingle
-import pkg_resources
-
-__version__ = pkg_resources.get_distribution('rrcf').version
--- setup.py
+++ setup.py
@@ -11,6 +11,6 @@ setup(name='rrcf',
       packages=["rrcf"],
       license="MIT",
       install_requires=[
-          'numpy<2.0.0',
+          'numpy>=2.4.4',
       ]
       )
